00257169efa3fba22377d2f308b4d438f834ea55
Demonassit <demonassit@hotmail.com>
2020-08-04 08:57:02 -0500

    Agrego los metodos del detalle de la venta

--- src/java/Controlador/Venta.java
+++ src/java/Controlador/Venta.java
@@ -11,4 +11,46 @@ package Controlador;
  */
 public class Venta {
     
+    private int venta_Codigo, usuario_Codigo;
+    private String venta_Fecha;
+    private double venta_TotalPagar;
+    
+    public Venta(){
+    
+    }
+
+    public int getVenta_Codigo() {
+        return venta_Codigo;
+    }
+
+    public void setVenta_Codigo(int venta_Codigo) {
+        this.venta_Codigo = venta_Codigo;
+    }
+
+    public int getUsuario_Codigo() {
+        return usuario_Codigo;
+    }
+
+    public void setUsuario_Codigo(int usuario_Codigo) {
+        this.usuario_Codigo = usuario_Codigo;
+    }
+
+    public String getVenta_Fecha() {
+        return venta_Fecha;
+    }
+
+    public void setVenta_Fecha(String venta_Fecha) {
+        this.venta_Fecha = venta_Fecha;
+    }
+
+    public double getVenta_TotalPagar() {
+        return venta_TotalPagar;
+    }
+
+    public void setVenta_TotalPagar(double venta_TotalPagar) {
+        this.venta_TotalPagar = venta_TotalPagar;
+    }
+    
+    
+    
 }
