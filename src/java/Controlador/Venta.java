48a78ff527ef4c1095a99f9983b2e44366072d94
Demonassit <demonassit@hotmail.com>
2020-08-05 08:39:30 -0500

    se agrego los detalles de la venta con sus metodos automaticos

--- src/java/Controlador/Venta.java
+++ src/java/Controlador/Venta.java
@@ -9,6 +9,11 @@ package Controlador;
  *
  * @author demon
  */
+
+import java.sql.*;
+import java.util.*;
+
+
 public class Venta {
     
     private int venta_Codigo, usuario_Codigo;
@@ -18,6 +23,138 @@ public class Venta {
     public Venta(){
     
     }
+    
+    //obtener de forma automatica la ultima venta generada, obtener el ultimo codigo vendido
+    
+    private int ultimoCodigoInserto(Connection con){
+    
+            int codigo = 0;
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+            
+            try{
+                
+                String q = "select MAX(venta_Codigo) as Codigo from Venta";
+                ps = con.prepareStatement(q);
+                rs = ps.executeQuery();
+                //buscar la ultima venta
+                while(rs.next()){
+                    codigo = rs.getInt("Codigo");
+                    break;
+                }
+                
+            
+            }catch(SQLException e){
+                System.out.println("Error al conectar con la Venta");
+                System.out.println(e.getMessage());
+                System.out.println(e.getStackTrace());
+            
+            }finally{
+                try{
+                    
+                    ps.close();
+                    rs.close();
+                
+                }catch(SQLException e){
+                    
+                    System.out.println(e.getStackTrace());
+                
+                }
+            }
+        return codigo;
+    }
+    
+    
+    //registro de la venta, para que se lance la actualizacion de los productos vendidos
+    public boolean registrarVenta(Venta v, Vector<DetalleVenta> ldv){
+        
+        boolean registro = false;
+        Connection con = null;
+        PreparedStatement ps = null;
+        try{
+        
+            con = Conexion.getConnection();
+            String q = "insert into Venta(venta_Fecha, usuario_Codigo, venta_Total) values (?, ?, ?)";
+            ps = con.prepareStatement(q);
+            //enviamos los parametros
+            ps.setString(1, v.getVenta_Fecha());
+            ps.setInt(2, v.getUsuario_Codigo());
+            ps.setDouble(3, v.getVenta_TotalPagar());
+            //hay que saber si se registro la venta
+            if(ps.executeUpdate() == 1){
+                //el registro de la venta
+                int codigo = this.ultimoCodigoInserto(con);
+                //bandera pq se acciona al existir una accion booleana
+                registro = this.registrarDetalleVenta(codigo, ldv, con);
+            
+            }else{
+                //cuando no ocurre
+                registro = false;
+            }
+        
+        }catch(SQLException e){
+            
+            System.out.println("Error al registrar la venta");
+            System.out.println(e.getMessage());
+            System.out.println(e.getStackTrace());
+            registro = false;
+        
+        }finally{
+            try{
+                
+                ps.close();
+                con.close();
+            
+            }catch(SQLException e){
+                System.out.println(e.getStackTrace());
+            }
+        }
+        return registro;
+    }
+    
+    //registro de la venta, aqui inserto los detalles de la venta, es automatico
+    
+    private boolean registrarDetalleVenta(int codigo, Vector<DetalleVenta> ldv, Connection con){
+        
+        boolean registro = false;
+        PreparedStatement ps = null;
+        try{
+            //recorrer toda la lista de productos que se van a vender
+            for(DetalleVenta dv : ldv){
+                
+                String q = "insert into detalleventa values (?,?,?,?,?)";
+                ps = con.prepareStatement(q);
+                ps.setInt(1, codigo);
+                ps.setInt(2, dv.getDetVenta_Item());
+                ps.setInt(3, dv.getProducto_Codigo());
+                ps.setInt(4, dv.getDetVenta_Cantidad());
+                ps.setDouble(5, dv.getDetVenta_Subtotal());
+                if(ps.executeUpdate() == 1){
+                    registro = true;
+                }else{
+                    registro = false;
+                    break;
+                }
+            }
+        
+        }catch(SQLException e){
+            System.out.println("Error al insertar el detalle de la venta");
+            System.out.println(e.getMessage());
+            System.out.println(e.getStackTrace());
+            registro = false;
+        
+        }finally{
+            try{
+                ps.close();
+            
+            }catch (SQLException e){
+                System.out.println(e.getStackTrace());
+            }
+        }
+        return registro;
+    
+    }
+    
 
     public int getVenta_Codigo() {
         return venta_Codigo;
